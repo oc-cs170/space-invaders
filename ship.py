f4be21892ff73d1353bad6d17f4d4e7d33ea7bae
Lindsey Winters <lwinters@olivetcollege.edu>
2013-02-12 20:10:41 -0500

    adding bullets

--- ship.py
+++ ship.py
@@ -21,7 +21,7 @@ class Ship(pygame.sprite.Sprite):
         
         self.rect = pygame.Rect((0, 0), self.image.get_size())
         self.rect.centerx = self.screen_width / 2
-        self.rect.bottom = self.screen_height - (self.rect.height)
+        self.rect.bottom = self.screen_height - self.rect.height
 
         self.move = 0
 
@@ -30,4 +30,46 @@ class Ship(pygame.sprite.Sprite):
         # Ship moves left and right here
         self.rect.left += self.move
         self.rect.left = min(max(self.rect.left, 0), self.screen_width - self.rect.width)
+
+class Bullet(pygame.sprite.Sprite):
+
+    bullet = pygame.Surface((3,3))
+    bullet.set_colorkey(pygame.Color('black'))
+    pygame.draw.circle(bullet, pygame.Color('white'), (2, 2), 2)
+
+    def __init__(self, location, ship):
+        pygame.sprite.Sprite.__init__(self)
+        if ship == 
+
+
+
+
+
+
+
+
+
+
+class Bullet(pygame.sprite.Sprite):
+    
+    bullet = pygame.Surface((4,4))
+    bullet.set_colorkey(pygame.Color('black'))
+    pygame.draw.circle(bullet, pygame.Color('white'), (2, 2), 2)
+    
+    def __init__(self, location, ship):
+        pygame.sprite.Sprite.__init__(self)
+        if ship == 'invader':
+            self.yv = 5
+        elif ship == 'hero':
+            self.yv = -5
+
+        self.image = self.bullet
+        self.rect = pygame.Rect((0,0), self.image.get_size())
+        self.rect.midbottom = location
+
+
+
+
+    def update(self):
+        self.rect.centery += self.yv
 
